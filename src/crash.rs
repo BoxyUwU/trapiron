986e55e3f38675cb975aed662a8ae9ff0e1d2af9
Boxy <rust@boxyuwu.dev>
2025-03-13 17:16:35 +0000

    Remove object lifetime cast

--- src/crash.rs
+++ src/crash.rs
@@ -183,6 +183,12 @@ mod context {
 
                 if let Some(entries) = &mut context.entries {
                     let x: &mut Self = self.get_unchecked_mut();
+                    // SAFETY: We extend the lifetime of `Self` to `'static`, I don't know
+                    // if this is sound.
+                    let x = std::mem::transmute::<
+                        *mut (dyn EntryLike + '_),
+                        *mut (dyn EntryLike + 'static),
+                    >(x);
                     entries.push(x as *mut Self as *mut dyn EntryLike);
                 } else {
                     // Fail silently: crash context has been forever disabled for this thread
